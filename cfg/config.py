7bf5854a9117c4883ba9fa1c8476f9e74ec7d164
Timothy <tzw202623@yahoo.com>
2023-10-19 01:38:28 -0700

    Specify which globals are dataframe headers

--- cfg/config.py
+++ cfg/config.py
@@ -2,38 +2,38 @@
 """
 
 ### Column headers for the dataframes
-OUTPUT_DRIVER_NAME_KEY = 'Driver'
-OUTPUT_DRIVER_PHONE_KEY = 'Driver Phone #'
-OUTPUT_DRIVER_CAPACITY_KEY = 'Seats'
+OUTPUT_DRIVER_NAME_HDR = 'Driver'
+OUTPUT_DRIVER_PHONE_HDR = 'Driver Phone #'
+OUTPUT_DRIVER_CAPACITY_HDR = 'Seats'
 
-DRIVER_TIMESTAMP_KEY = 'Timestamp'
-DRIVER_NAME_KEY = 'Name'
-DRIVER_PHONE_KEY = 'Phone Number'
-DRIVER_CAPACITY_KEY = 'Number of Seats in Car (not including you)'
+DRIVER_TIMESTAMP_HDR = 'Timestamp'
+DRIVER_NAME_HDR = 'Name'
+DRIVER_PHONE_HDR = 'Phone Number'
+DRIVER_CAPACITY_HDR = 'Number of Seats in Car (not including you)'
 
-RIDER_TIMESTAMP_KEY = 'Timestamp'
-RIDER_NAME_KEY = 'Rider'
-RIDER_PHONE_KEY = 'Rider Phone #'
-RIDER_LOCATION_KEY = 'Location'
-RIDER_FRIDAY_KEY = 'Friday'
-RIDER_SUNDAY_KEY = 'Sunday'
-RIDER_NOTES_KEY = 'Notes'
+RIDER_TIMESTAMP_HDR = 'Timestamp'
+RIDER_NAME_HDR = 'Rider'
+RIDER_PHONE_HDR = 'Rider Phone #'
+RIDER_LOCATION_HDR = 'Location'
+RIDER_FRIDAY_HDR = 'Friday'
+RIDER_SUNDAY_HDR = 'Sunday'
+RIDER_NOTES_HDR = 'Notes'
 
-PERMANENT_RIDER_TIMESTAMP_KEY = 'Timestamp'
-PERMANENT_RIDER_NAME_KEY = 'Full Name:'
-PERMANENT_RIDER_PHONE_KEY = 'Phone Number: '
-PERMANENT_RIDER_LOCATION_KEY = 'Where should we pick you up?'
-PERMANENT_RIDER_FRIDAY_KEY = 'Which service(s) do you need a permanent ride for? [Friday Night Bible Study | 6:30 pm]'
-PERMANENT_RIDER_SUNDAY_KEY = 'Which service(s) do you need a permanent ride for? [Sunday Service | 8:30 am/10:45 am]'
-PERMANENT_RIDER_NOTES_KEY = 'Other Notes'
+PERMANENT_RIDER_TIMESTAMP_HDR = 'Timestamp'
+PERMANENT_RIDER_NAME_HDR = 'Full Name:'
+PERMANENT_RIDER_PHONE_HDR = 'Phone Number: '
+PERMANENT_RIDER_LOCATION_HDR = 'Where should we pick you up?'
+PERMANENT_RIDER_FRIDAY_HDR = 'Which service(s) do you need a permanent ride for? [Friday Night Bible Study | 6:30 pm]'
+PERMANENT_RIDER_SUNDAY_HDR = 'Which service(s) do you need a permanent ride for? [Sunday Service | 8:30 am/10:45 am]'
+PERMANENT_RIDER_NOTES_HDR = 'Other Notes'
 
-WEEKLY_RIDER_TIMESTAMP_KEY = 'Timestamp'
-WEEKLY_RIDER_NAME_KEY = 'Full Name'
-WEEKLY_RIDER_PHONE_KEY = 'Phone Number '
-WEEKLY_RIDER_LOCATION_KEY = 'Where should we pick you up from?'
-WEEKLY_RIDER_FRIDAY_KEY = 'Friday Night Bible Study (Friday @7pm) (Rides from Campus will be provided at Peterson Loop at 6:30 pm)'
-WEEKLY_RIDER_SUNDAY_KEY = 'Sunday Service '
-WEEKLY_RIDER_NOTES_KEY = 'Additional Comments / Questions / Concerns'
+WEEKLY_RIDER_TIMESTAMP_HDR = 'Timestamp'
+WEEKLY_RIDER_NAME_HDR = 'Full Name'
+WEEKLY_RIDER_PHONE_HDR = 'Phone Number '
+WEEKLY_RIDER_LOCATION_HDR = 'Where should we pick you up from?'
+WEEKLY_RIDER_FRIDAY_HDR = 'Friday Night Bible Study (Friday @7pm) (Rides from Campus will be provided at Peterson Loop at 6:30 pm)'
+WEEKLY_RIDER_SUNDAY_HDR = 'Sunday Service '
+WEEKLY_RIDER_NOTES_HDR = 'Additional Comments / Questions / Concerns'
 
 
 ### For parsing the responses for attending the Friday/Sunday services
@@ -41,8 +41,24 @@ PERMANENT_RIDE_THERE_KEYWORD = 'yes'
 WEEKLY_RIDE_THERE_KEYWORD = 'there'
 RIDE_THERE_KEYWORD = 'yes'
 
-DRIVER_OPENINGS_KEY = 'Open seats'
-DRIVER_ROUTE_KEY = 'Locations'
+DRIVER_OPENINGS_HDR = 'Open seats'
+DRIVER_ROUTE_HDR = 'Locations'
+
+# File paths
+import os
+DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pickle')
+CFG_PATH = os.path.dirname(os.path.realpath(__file__))
+MAP_FILE = os.path.join(CFG_PATH, 'map.txt')
+IGNORE_DRIVERS_FILE = os.path.join(CFG_PATH, 'ignore_drivers.txt')
+IGNORE_RIDERS_FILE = os.path.join(CFG_PATH, 'ignore_riders.txt')
+SERVICE_ACCT_FILE = os.path.join(CFG_PATH, 'service_account.json')
+SHEET_IDS_FILE = os.path.join(CFG_PATH, 'sheet_ids.json')
+
+# Sheet ID keys
+PERMANENT_SHEET_KEY = 'permanent'
+WEEKLY_SHEET_KEY = 'weekly'
+DRIVER_SHEET_KEY = 'drivers'
+OUTPUT_SHEET_KEY = 'out'
 
 # The number of openings required for a car to freely pick up from a neighboring location
 GROUPING_THRESHOLD = 'threshold'
@@ -53,26 +69,10 @@ GLOBALS = {                     # Use a dict in order to modify the global var l
 # Route codes
 LOC_KEY_ELSEWHERE = 'ELSEWHERE'
 LOC_NONE = 0b0
-loc_map = {
+LOC_MAP = {
     LOC_KEY_ELSEWHERE: LOC_NONE
 }
 
-# File paths
-import os
-DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pickle')
-CFG_PATH = os.path.dirname(os.path.realpath(__file__))
-MAP_FILE = os.path.join(CFG_PATH, 'map.txt')
-IGNORE_DRIVERS_FILE = os.path.join(CFG_PATH, 'ignore_drivers.txt')
-IGNORE_RIDERS_FILE = os.path.join(CFG_PATH, 'ignore_riders.txt')
-SERVICE_ACCT_FILE = os.path.join(CFG_PATH, 'service_account.json')
-SHEET_ID_FILE = os.path.join(CFG_PATH, 'sheet_ids.json')
-
-# Sheet ID keys
-PERMANENT_SHEET_KEY = 'permanent'
-WEEKLY_SHEET_KEY = 'weekly'
-DRIVER_SHEET_KEY = 'drivers'
-OUTPUT_SHEET_KEY = 'out'
-
 # Lists to be filled later
-ignored_drivers = []
-ignored_riders = []
+IGNORED_DRIVERS = []
+IGNORED_RIDERS = []
--- lib/assignments.py
+++ lib/assignments.py
@@ -14,8 +14,8 @@ def assign(drivers_df: pd.DataFrame, riders_df: pd.DataFrame) -> pd.DataFrame:
 
     PRECONDITION: add_temporaries must have been called on drivers_df.
     """
-    riders_df.sort_values(by=RIDER_LOCATION_KEY, inplace=True, key=lambda col: col.apply(lambda loc: loc_map.get(loc, loc_map[LOC_KEY_ELSEWHERE])))
-    out = pd.concat([pd.DataFrame(columns=[OUTPUT_DRIVER_NAME_KEY, OUTPUT_DRIVER_PHONE_KEY, OUTPUT_DRIVER_CAPACITY_KEY]), riders_df[[RIDER_NAME_KEY, RIDER_PHONE_KEY, RIDER_LOCATION_KEY, RIDER_NOTES_KEY]]], axis='columns')
+    riders_df.sort_values(by=RIDER_LOCATION_HDR, inplace=True, key=lambda col: col.apply(lambda loc: LOC_MAP.get(loc, LOC_MAP[LOC_KEY_ELSEWHERE])))
+    out = pd.concat([pd.DataFrame(columns=[OUTPUT_DRIVER_NAME_HDR, OUTPUT_DRIVER_PHONE_HDR, OUTPUT_DRIVER_CAPACITY_HDR]), riders_df[[RIDER_NAME_HDR, RIDER_PHONE_HDR, RIDER_LOCATION_HDR, RIDER_NOTES_HDR]]], axis='columns')
 
     logging.debug('Drivers')
     logging.debug(drivers_df)
@@ -24,11 +24,11 @@ def assign(drivers_df: pd.DataFrame, riders_df: pd.DataFrame) -> pd.DataFrame:
     logging.debug('Assigning started')
 
     for r_idx in out.index:
-        rider_loc = loc_map.get(out.at[r_idx, RIDER_LOCATION_KEY], loc_map[LOC_KEY_ELSEWHERE])
+        rider_loc = LOC_MAP.get(out.at[r_idx, RIDER_LOCATION_HDR], LOC_MAP[LOC_KEY_ELSEWHERE])
 
-        if rider_loc == loc_map[LOC_KEY_ELSEWHERE]:
+        if rider_loc == LOC_MAP[LOC_KEY_ELSEWHERE]:
             #TODO: do not assign for now
-            logging.debug(f'\t{out.at[r_idx, RIDER_NAME_KEY]} is not from a prerecorded location, assigning skipped')
+            logging.debug(f'\t{out.at[r_idx, RIDER_NAME_HDR]} is not from a prerecorded location, assigning skipped')
             continue
 
         is_matched = False
@@ -39,7 +39,7 @@ def assign(drivers_df: pd.DataFrame, riders_df: pd.DataFrame) -> pd.DataFrame:
                 _add_rider(out, r_idx, drivers_df, d_idx)
                 is_matched = True
                 break
-            if _is_nearby_dist(driver, rider_loc, 1) and driver[DRIVER_OPENINGS_KEY] >= GLOBALS[GROUPING_THRESHOLD]:
+            if _is_nearby_dist(driver, rider_loc, 1) and driver[DRIVER_OPENINGS_HDR] >= GLOBALS[GROUPING_THRESHOLD]:
                 # If a driver is one spot away and are not going "out of their way", that driver will get assigned.
                 _add_rider(out, r_idx, drivers_df, d_idx)
                 is_matched = True
@@ -105,12 +105,12 @@ def assign_friday(drivers_df: pd.DataFrame, riders_df: pd.DataFrame) -> pd.DataF
 def _add_rider(out: pd.DataFrame, r_idx: int, drivers_df: pd.DataFrame, d_idx: int):
     """Assigns rider to driver and updates driver openings and locations.
     """
-    out.at[r_idx, OUTPUT_DRIVER_NAME_KEY] = drivers_df.at[d_idx, DRIVER_NAME_KEY]
-    out.at[r_idx, OUTPUT_DRIVER_PHONE_KEY] = drivers_df.at[d_idx, DRIVER_PHONE_KEY]
-    out.at[r_idx, OUTPUT_DRIVER_CAPACITY_KEY] = int(drivers_df.at[d_idx, DRIVER_CAPACITY_KEY])  # Chose not to include total seats
-    rider_loc = loc_map.get(out.at[r_idx, RIDER_LOCATION_KEY], loc_map[LOC_KEY_ELSEWHERE])
-    drivers_df.at[d_idx, DRIVER_OPENINGS_KEY] -= 1
-    drivers_df.at[d_idx, DRIVER_ROUTE_KEY] |= rider_loc
+    out.at[r_idx, OUTPUT_DRIVER_NAME_HDR] = drivers_df.at[d_idx, DRIVER_NAME_HDR]
+    out.at[r_idx, OUTPUT_DRIVER_PHONE_HDR] = drivers_df.at[d_idx, DRIVER_PHONE_HDR]
+    out.at[r_idx, OUTPUT_DRIVER_CAPACITY_HDR] = int(drivers_df.at[d_idx, DRIVER_CAPACITY_HDR])  # Chose not to include total seats
+    rider_loc = LOC_MAP.get(out.at[r_idx, RIDER_LOCATION_HDR], LOC_MAP[LOC_KEY_ELSEWHERE])
+    drivers_df.at[d_idx, DRIVER_OPENINGS_HDR] -= 1
+    drivers_df.at[d_idx, DRIVER_ROUTE_HDR] |= rider_loc
 
 
 def _is_nearby_dist(driver: pd.Series, rider_loc: int, dist: int) -> bool:
@@ -128,17 +128,17 @@ def _is_there_or_open(driver: pd.Series, rider_loc: int) -> bool:
 def _has_opening(driver: pd.Series) -> bool:
     """Checks if driver has space to take a rider.
     """
-    return driver[DRIVER_OPENINGS_KEY] > 0
+    return driver[DRIVER_OPENINGS_HDR] > 0
 
 
 def _is_free(driver: pd.Series) -> bool:
     """Checks if driver is completely free (no riders assigned).
     """
-    return driver[DRIVER_ROUTE_KEY] == LOC_NONE
+    return driver[DRIVER_ROUTE_HDR] == LOC_NONE
 
 
 def _is_intersecting(driver: pd.Series, rider_loc: int) -> bool:
     """Checks if a driver route intersects with a rider's location.
     """
-    driver_loc = driver[DRIVER_ROUTE_KEY]
+    driver_loc = driver[DRIVER_ROUTE_HDR]
     return (driver_loc & rider_loc) != 0
